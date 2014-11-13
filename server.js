39ccc7080bfb07e78b717ff09183725d5a6495a6
Andrew <luzkovskyandrey@gmail.com>
2014-11-13 08:05:57 +0300

    add gitignore, fix path

--- server.js
+++ server.js
@@ -12,16 +12,14 @@ app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 var formidable = require('formidable')
-var util = require('util');
 
 app.post('/upload', function(req, res){
 	var form = new formidable.IncomingForm();
 	form.uploadDir = path.join(__dirname, 'public', 'img')
 
     form.parse(req, function(err, fields, files) {
-    	var p = files.file.path.split('\\');
 
-     	var fileName = p[p.length - 1];
+     	var fileName = path.basename(files.file.path)
     	var src = '/img/' + fileName;
 
     	res.writeHead(200, {'content-type': 'text/plain'});
